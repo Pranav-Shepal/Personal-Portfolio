adbfbd0fc01b2cc6f8896c86cbb26d7cf131043f
Pranav-Shepal <143519184+Pranav-Shepal@users.noreply.github.com>
2025-07-09 16:30:53 +0530

    Update index.js

--- Frontend/index.js
+++ Frontend/index.js
@@ -67,7 +67,7 @@ document.getElementById("contactform").addEventListener("submit", async function
     };
 
     try {
-        let response = await fetch("https://pranav-portfolio-xt0l.onrender.com/api/send-message", {
+        let response = await fetch("https://personal-portfolio-o395.onrender.com/api/send-message", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
